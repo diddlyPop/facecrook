246a04f77fff2dc584d5c6e4918824cc42d0564c
= <kyleguss@icloud.com>
2019-09-16 18:34:32 -0700

    outline

---
+++ facecrook.py
@@ -0,0 +1,21 @@
+"""
+facecrook.py
+Kyle Guss & Jesse Mazzella
+
+proof of concept
+packet sniffing for pictures of faces
+"""
+
+# import
+
+# sniff
+
+# construct PCAP file
+
+# search PCAP file for images
+
+# feed images to image classifier to determine if face
+
+
+
+
