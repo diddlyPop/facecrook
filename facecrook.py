fbcd27b75215100e7d9cda7fe7987832654aec15
= <kyleguss@icloud.com>
2019-11-01 16:48:47 -0700

    pulls images from network traffic

--- facecrook.py
+++ facecrook.py
@@ -12,17 +12,13 @@ import zlib, scipy, numpy, os, re
 
 def get_http_headers(g_http_payload):
     try:
-
-        g_headers_raw = g_http_payload[:g_http_payload.index("\\r\\n\\r\\n")+4]
-        if g_headers_raw is not None:
-            print("raw: " + g_headers_raw)
-        g_headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\\r\\n", g_headers_raw))
-        print("headders: " + str(g_headers.keys()))
+        g_headers_raw = g_http_payload[g_http_payload.index(b"HTTP/1.1"):g_http_payload.index(b"\r\n\r\n") + 2]
+        g_headers_parsed = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", g_headers_raw.decode("utf8")))
     except Exception as e:
         return None
-    if 'Content-Type' not in g_headers:
+    if 'Content-Type' not in g_headers_parsed.keys():
         return None
-    return g_headers
+    return g_headers_parsed
 
 
 def extract_image(headers, http_payload):
@@ -32,17 +28,17 @@ def extract_image(headers, http_payload):
     try:
         if 'image' in headers['Content-Type']:
             image_type = headers['Content-Type'].split('/')[1]
-            image = http_payload[http_payload.index('\\r\\n\\r\\n')+8:]
-            print("image: " + image)
+            image = http_payload[http_payload.index(b"\r\n\r\n")+4:]
             try:
                 if 'Content-Encoding' in headers.keys():
                     if headers['Content-Encoding'] == 'gzip':
                         image = zlib.decompress(image, 16+zlib.MAX_WBITS)
                     elif headers['Content-Encoding'] == 'deflate':
                         image = zlib.decompress(image)
-            except:
-                pass
-    except:
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
         return None, None
     return image, image_type
 
@@ -68,22 +64,23 @@ carved_images = 0
 PIC_DIR = f"{os.environ['USERPROFILE']}/Desktop/Facecrook/Pictures" # pic output path
 
 for session in sessions:
-    http_payload = ''
+    http_payload = b''
     for packet in sessions[session]:
         try:
             if packet[TCP].dport == 80 or packet[TCP].sport == 80:
-                http_payload += str(packet[TCP].payload)
+                http_payload += bytes(packet[TCP].payload)
         except Exception as e:
             pass
         headers = get_http_headers(http_payload)
         if headers is None:
+            print("headers is none")
             continue
         image, image_type = extract_image(headers, http_payload)
         if image is not None and image_type is not None:
             print("FOUND IMAGE")
-            file_name = '%s-pic_carver_%d.%s' % ("packets", carved_images, image_type)
+            file_name = '{0}-pic_carver_{1}.{2}'.format("packets", carved_images, image_type)
             fd = open('%s/%s' % (PIC_DIR, file_name), 'wb')
-            fd.write(image[2:].encode())
+            fd.write(image)
             fd.close()
             carved_images += 1
 
